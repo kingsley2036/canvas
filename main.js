bd795260eb7fbd0659cb919ca41c48d59cd3668f
kingsley <kingsley2036@qq.com>
2019-07-13 21:38:07 +0800

    设备兼容调试

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
 autoSetCanvasSize(yyy)
-listenToMouse(yyy)
+listenToUser(yyy)
 
 
 
 // 画圆函数
 function drawCircle(x, y, radius) {
-    context.beginPath();
-    context.arc(x, y, radius, 0, Math.PI * 2)
-    context.fill()
-  }
-  // 画线函数
+  context.beginPath();
+  context.arc(x, y, radius, 0, Math.PI * 2)
+  context.fill()
+}
+// 画线函数
 function drawLine(x1, y1, x2, y2) {
   context.beginPath()
   context.moveTo(x1, y1) //起点
@@ -23,79 +23,136 @@ function drawLine(x1, y1, x2, y2) {
 
 var using = false
 var lastPoint = {
-    x: undefined,
-    y: undefined
-  }
+  x: undefined,
+  y: undefined
+}
+
 
 // 监听鼠标
-function listenToMouse(canvas){
-   // 点击
-canvas.onmousedown = function(aaa) {
-    var x = aaa.clientX
-    var y = aaa.clientY
-    using = true
-    if (eraserEnable) {
-      
-      context.clearRect(x - 5, y - 5, 10, 10)
+function listenToUser(canvas) {
+  // 特性检测
+  if (document.body.ontouchstart !== undefined) {
+    //触屏设备
+    canvas.ontouchstart=function(aaa){
+      var x = aaa.touches[0].clientX
+      var y = aaa.touches[0].clientY
+      console.log(x,y)
+      using = true
+      if (eraserEnable) {
 
-    } else {
-      
-
-      lastPoint = {
-        "x": x,
-        "y": y
-      }
-    }
-
-    //drawCircle(x, y, 3)
-  }
-  // 移动
-canvas.onmousemove = function(aaa) {
-    var x = aaa.clientX
-    var y = aaa.clientY
-    if (eraserEnable) {
-      if (using) {
         context.clearRect(x - 5, y - 5, 10, 10)
-      }
+
+      } else {
 
 
-    } else {
-      if (using) {
-
-        var newPoint = {
+        lastPoint = {
           "x": x,
           "y": y
         }
-
-        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-        lastPoint = newPoint
-
       }
+
+    }
+canvas.ontouchmove=function(aaa){
+  var x = aaa.touches[0].clientX
+  var y = aaa.touches[0].clientY
+  if (eraserEnable) {
+    if (using) {
+      context.clearRect(x - 5, y - 5, 10, 10)
+    }
+
+
+  } else {
+    if (using) {
+
+      var newPoint = {
+        "x": x,
+        "y": y
+      }
+
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+      lastPoint = newPoint
 
     }
 
   }
-  // 松开
-canvas.onmouseup = function(aaa) {
-    using = false
+    }
+    canvas.ontouchend=function(aaa){
+      using = false
+    }
+  } else {
+
+    //非触屏设备
+    // 点击
+    canvas.onmousedown = function (aaa) {
+      var x = aaa.clientX
+      var y = aaa.clientY
+      using = true
+      if (eraserEnable) {
+
+        context.clearRect(x - 5, y - 5, 10, 10)
+
+      } else {
+
+
+        lastPoint = {
+          "x": x,
+          "y": y
+        }
+      }
+
+      //drawCircle(x, y, 3)
+    }
+    // 移动
+    canvas.onmousemove = function (aaa) {
+      var x = aaa.clientX
+      var y = aaa.clientY
+      if (eraserEnable) {
+        if (using) {
+          context.clearRect(x - 5, y - 5, 10, 10)
+        }
+
+
+      } else {
+        if (using) {
+
+          var newPoint = {
+            "x": x,
+            "y": y
+          }
+
+          drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+          lastPoint = newPoint
+
+        }
+
+      }
+
+    }
+    // 松开
+    canvas.onmouseup = function (aaa) {
+      using = false
+    }
   }
+
+
+
 }
- 
-  /******/
+
+/******/
 var eraserEnable = false
-eraser.onclick = function() {
+eraser.onclick = function () {
   eraserEnable = true
-  actions.className="actions x" 
+  actions.className = "actions x"
 }
-brush.onclick = function(){
+brush.onclick = function () {
   eraserEnable = false
-  actions.className="actions" 
+  actions.className = "actions"
 }
 
 function autoSetCanvasSize(canvas) {
   setCanvasSize()
 
-  window.onresize = function() {
+  window.onresize = function () {
     setCanvasSize()
   }
 
